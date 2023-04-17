ffb6d1d97fefce850b6d8344d342a2910c7764f1
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2023-04-17 14:57:07 -0500

    Add playRound()

--- main.js
+++ main.js
@@ -17,6 +17,36 @@ function getComputerChoice() {
     }
 }
 
-for (i=0; i < 10; i++) {
-    console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === computerSelection) {
+        return "tie";
+    } else if (playerSelection === "rock") {
+        switch(computerSelection) {
+            case "scissors":
+                return "win";
+            case "paper":
+                return "lose";
+        }
+    } else if (playerSelection === "scissors") {
+        switch(computerSelection) {
+            case "paper":
+                return "win";
+            case "rock":
+                return "lose";
+        }
+    } else if (playerSelection === "paper") {
+        switch(computerSelection) {
+            case "rock":
+                return "win";
+            case "scissors":
+                return "lose";
+        }
+    }
 }
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(computerSelection);
+console.log(playRound(playerSelection, computerSelection));
